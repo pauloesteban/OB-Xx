75e6da26ec464a9da7ee6bf446162ab61eaf9c8a
gitterdude <gitterdude@internet.the>
2023-01-14 00:14:19 +0100

    Added _inst.sh

--- _inst.sh
+++ _inst.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 Ver=`git describe --tags`
-Opt=/opt/OB-Xx-${Ver}/lib/lv2
-Loc=/usr/local/lib/lv2
-Build=Builds/LinuxMakefile/build/OB-Xx.lv2
+Opt=/opt/OB-Xx-${Ver}
+Loc=/usr/local
+Build=Builds/LinuxMakefile/build/
 
 echo ""
 echo " 1) $Opt"
-echo " 2) /usr/local/lib/lv2"
-echo " 3) ~/.lv2"
+echo " 2) $Loc"
+echo " 3) ~/  [.lv2/.vst3/.bin]"
 echo ""
 echo -n " Which of the folders above to install to? -> "
 read Inst
 case $Inst in 
- 1) mkdir -p $Opt && cp -a $Build $Opt || echo "ERROR; Couldn't create $Opt. Wrong permissions?"  ;;
- 2) test -d $Loc && touch -d $Loc/_sadflk3453j45 && rm -f $Loc/_sadflk3453j45 && cp -a $Build $Loc/ || echo "ERROR; Couldn't copy to $Loc . Wrong permissions?"  ;;
- 3) test -d ~/.lv2 || mkdir ~/.lv2 ;  cp -a $Build ~/.lv2 ;;
+ 1) mkdir -p $Opt/lib/lv2 && test ! -d $Opt/lib/lv2 && echo "ERROR; Couldn't create $Opt. Wrong permissions?" && exit 2
+    cp -a $Build/OB-Xx.lv2 $Opt/lib/lv2
+    test -d $Build/OB-Xx.vst3/Contents && mkdir -p $Opt/lib/vst3 && cp -a $Build/OB-Xx.vst3 $Opt/lib/vst3
+    test -f $Build/OB-Xx && mkdir -p $Opt/bin && cp -a $Build/OB-Xx $Opt/bin
+    ;;
+ 2) test -d $Loc/lib/lv2 || mkdir $Loc/lib/lv2 >& /dev/null
+    test ! -w $Loc/lib/lv2 && echo "ERROR; Couldn't write to $Loc/lib/lv2 . Wrong permissions?" && exit 3 
+    cp -a $Build/OB-Xx.lv2  $Loc/lib/lv2
+    if [ -d $Build/OB-Xx.vst3/Contents ]; then 
+      test -d $Loc/lib/vst3 || mkdir $Loc/lib/vst3
+      cp -a $Build/OB-Xx.vst3  $Loc/lib/vst3
+    fi
+    test -f $Build/OB-Xx && cp -a $Build/OB-Xx  $Loc/bin
+    ;;
+ 3) test -d ~/.lv2 || mkdir ~/.lv2 ;  cp -a $Build/OB-Xx.lv2 ~/.lv2
+    test -d $Build/OB-Xx.vst3/Contents && mkdir ~/.vst3 >& /dev/null ; cp -a $Build/OB-Xx.vst3 
+    test -f $Build/OB-Xx && mkdir ~/.bin >& /dev/null ; cp -a $Build/OB-Xx  ~/.bin 
+    ;;
  *) echo "ERROR: Wrong choice. Run $0 again manually"
 esac
 
