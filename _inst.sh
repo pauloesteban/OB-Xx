d0888e96adf125d95e1270c0a4f74a88a085bc66
gitterdude <gitterdude@internet.the>
2023-01-14 00:34:58 +0100

    Fixed _inst.sh info

--- _inst.sh
+++ _inst.sh
@@ -11,14 +11,15 @@ echo " 3) ~/  [.lv2/.vst3/.bin]"
 echo ""
 echo -n " Which of the folders above to install to? -> "
 read Inst
+echo ""
 case $Inst in 
- 1) mkdir -p $Opt/lib/lv2 && test ! -d $Opt/lib/lv2 && echo "ERROR; Couldn't create $Opt. Wrong permissions?" && exit 2
+ 1) mkdir -p $Opt/lib/lv2 && test ! -d $Opt/lib/lv2 && echo "ERROR; Couldn't create $Opt. Wrong permissions?" && echo "Try su/sudo to root and run $0 manually" && exit 2
     cp -a $Build/OB-Xx.lv2 $Opt/lib/lv2
     test -d $Build/OB-Xx.vst3/Contents && mkdir -p $Opt/lib/vst3 && cp -a $Build/OB-Xx.vst3 $Opt/lib/vst3
     test -f $Build/OB-Xx && mkdir -p $Opt/bin && cp -a $Build/OB-Xx $Opt/bin
     ;;
  2) test -d $Loc/lib/lv2 || mkdir $Loc/lib/lv2 >& /dev/null
-    test ! -w $Loc/lib/lv2 && echo "ERROR; Couldn't write to $Loc/lib/lv2 . Wrong permissions?" && exit 3 
+    test ! -w $Loc/lib/lv2 && echo "ERROR; Couldn't write to $Loc/lib/lv2 . Wrong permissions?" && echo "Try su/sudo to root and run $0 manually" && exit 3 
     cp -a $Build/OB-Xx.lv2  $Loc/lib/lv2
     if [ -d $Build/OB-Xx.vst3/Contents ]; then 
       test -d $Loc/lib/vst3 || mkdir $Loc/lib/vst3
